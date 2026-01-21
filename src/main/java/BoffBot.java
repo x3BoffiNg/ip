38bfe6303a6910b930f3719779c191cd719ff488
Clara Ng <x3Clarawork@gmail.com>
2026-01-22 01:55:43 +0800

    Level-2: Add, List

--- src/main/java/BoffBot.java
+++ src/main/java/BoffBot.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BoffBot {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        List<String> inputList = new ArrayList<>();
 
         String logo = "------------------------\n"
                 + "Hello! I'm BoffBot\n"
@@ -21,11 +25,20 @@ public class BoffBot {
                 System.out.println("       " + input);
 
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("------------------------\n");
+                for (int i=0; i < inputList.size(); i++ ){
+                    System.out.println((i+1) +". " + inputList.get(i));
+                }
+
+            }
+            else{
+                inputList.add(input);
+                System.out.println("------------------------\n");
+                System.out.println("added: " +input);
+                System.out.println("------------------------\n");
             }
 
-            System.out.println("------------------------\n");
-            System.out.println("       " +input);
-            System.out.println("------------------------\n");
 
         }
 
