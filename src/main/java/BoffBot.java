cf480e1350c207f00d7ea4492e0217b0c87c3d46
Clara Ng <x3Clarawork@gmail.com>
2026-01-21 22:47:38 +0800

    Level-0: Rename to BoffBot, Greet, Exit

---
+++ src/main/java/BoffBot.java
@@ -0,0 +1,11 @@
+public class BoffBot {
+    public static void main(String[] args) {
+        String logo = "------------------------\n"
+                + "Hello! I'm BoffBot\n"
+                + "What can I do for you?\n";
+        String logo2 = "------------------------\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "------------------------\n";
+        System.out.println(logo + logo2);
+    }
+}
